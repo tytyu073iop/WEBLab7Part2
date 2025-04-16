7deb01e67d1b70602e970bfd86b1702da1a731b8
tytyu073iop <fpm.biryuk@bsu.by>
2025-04-16 09:11:22 +0300

    task 1 is ready

--- SPA.js
+++ SPA.js
@@ -22,42 +22,27 @@ function ErrorHandler(jqXHR,StatusStr,ErrorStr)
   alert(StatusStr+' '+ErrorStr);
 }
 
-window.onhashchange=switchToStateFromURLHash;
-
 // текущее состояние приложения
 // это Model из MVC
 var SPAState={};
 
-// фотографии, которые можно просмотреть
-var photos={
-  1 : { image:"Hilu3.jpg", comment:"собака Шарик" },
-  2 : { image:"Retriever3.jpg", comment:"собака Барбос" }
-};
+const buttonsIds = ["Plants", "Main"];
 
-// вызывается при изменении закладки УРЛа
 // а также при первом открытии страницы
-// читает новое состояние приложения из закладки УРЛа
-// и обновляет ВСЮ вариабельную часть веб-страницы
+// обновляет ВСЮ вариабельную часть веб-страницы
 // соответственно этому состоянию
 // это упрощённая реализация РОУТИНГА - автоматического выполнения нужных
 // частей кода в зависимости от формы URLа
 // "роутинг" и есть "контроллер" из MVC - управление приложением через URL
-function switchToStateFromURLHash() {
-  var URLHash=window.location.hash;
-  
-  // убираем из закладки УРЛа решётку
-  // (по-хорошему надо ещё убирать восклицательный знак, если есть)
-  var stateStr=URLHash.substr(1);
-  
-  if ( stateStr != "" ) { // если закладка непустая, читаем из неё состояние и отображаем
-    var parts=stateStr.split("_")
-    SPAState={ pagename: parts[0] }; // первая часть закладки - номер страницы
-  } else {
-    SPAState={pagename:'Main'}; // иначе показываем главную страницу
-  }
+function switchToState(state) {
+  SPAState.pagename = state;
   
   console.log('Новое состояние приложения:');
   console.log(SPAState);
+
+  for (let id of buttonsIds) {
+    document.getElementById(id).style.display = id === state ? "none" : "block";
+  }
 
   TestLoadData(SPAState.pagename)
   
@@ -66,4 +51,4 @@ function switchToStateFromURLHash() {
   
 }
 
-switchToStateFromURLHash();
+switchToState("Main")
--- index.js
+++ index.js
@@ -8,6 +8,12 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + "/" + "SPA.html")
 })
 
+const contentFolder = "content";
+
+app.get('/:page', (req, res) => {
+  res.sendFile(__dirname + `/${contentFolder}/${req.params.page}.html`);
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
